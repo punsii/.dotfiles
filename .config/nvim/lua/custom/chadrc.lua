fb53c3b6a696bf7dcc54114f73c90435d3d799ce
Paul Menhart <paul@menhart.net>
2024-02-26 19:17:09 +0100

    only manage home folder

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -6,7 +6,7 @@ local M = {}
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
 M.ui = {
-  theme = "everforest_light",
+  theme = "everforest",
   tabufline = {
     lazyload = false,
   },
