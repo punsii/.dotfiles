11c14c5648d6c8965c8476f2b1cf04d487be8600
Paul Menhart <paul@menhart.net>
2024-02-28 21:06:21 +0100

    i3 colors

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -4,11 +4,13 @@ local null_ls = require "null-ls"
 local opts = {
   sources = {
     null_ls.builtins.code_actions.statix,
-    null_ls.builtins.diagnostics.deadnix,
+
     -- null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.diagnostics.pylint,
     -- null_ls.builtins.diagnostics.ruff,
+    null_ls.builtins.diagnostics.deadnix,
+    null_ls.builtins.diagnostics.pylint,
     null_ls.builtins.diagnostics.statix,
+
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
     null_ls.builtins.formatting.nixpkgs_fmt,
