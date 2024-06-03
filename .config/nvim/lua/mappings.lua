b89631e4f88802c6863abb69d6e69a22d8764144
Paul Menhart <paul_menhart@genua.de>
2024-06-03 16:22:42 +0200

    merge mappings.lua

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,10 +1,120 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map(
+  {"n"},
+  "<leader>fk",
+  "<cmd> Telescope keymaps <CR>",
+  {desc = "Find keymaps"}
+)
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map(
+  {"n"},
+  "<leader>gd",
+  function()
+    if next(require("diffview.lib").views) == nil then
+      vim.cmd("DiffviewOpen")
+    else
+      vim.cmd("DiffviewClose")
+    end
+  end,
+  {desc = "Goto Definition"}
+)
+
+map(
+  {"n"},
+  "<leader>sh",
+  function()
+    require("gitsigns").stage_hunk()
+  end,
+  {desc = "Stage hunk"}
+)
+
+map(
+  {"n"},
+  "<leader>uh",
+  function()
+    require("gitsigns").undo_stage_hunk()
+  end,
+  {desc = "Unstage hunk"}
+)
+
+map(
+  {"n"},
+  "<leader>sh",
+  function()
+    require("gitsigns").stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+  end,
+  {desc =	"Stage hunk"}
+)
+map(
+  {"n"},
+  "<leader>uh",
+  function()
+    require("gitsigns").undo_stage_hunk()
+  end,
+  {desc =	"Unstage hunk"}
+)
+--XXX how is the plugin setting used after 2.5?
+--M.dap = {
+--	plugin = true,
+--	n = {
+--		["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>", "Run Python Tests" },
+--	},
+--}
+--
+--XXX how is the plugin setting used after 2.5?
+--M.dap_python = {
+--	plugin = true,
+--	n = {
+--		["<leader>dpr"] = {
+--			function()
+--				require("dap-python").test_method()
+--			end,
+--			"Run Python Tests",
+--		},
+--	},
+--}
+-- XXX Overwrites the default mapping
+-- M.general = {
+-- 	n = {
+-- 		["<leader>fm"] = {
+-- 			function()
+-- 				vim.lsp.buf.format({
+-- 					async = true,
+-- 					filter = function(client)
+-- 						return client.name == "null-ls"
+-- 					end,
+-- 				})
+-- 			end,
+-- 			"LSP formatting",
+-- 		},
+-- 	},
+map(
+  {"n"},
+  "<leader>tt",
+			function()
+				require("base46").toggle_theme()
+			end,
+  {desc = "Toggle Theme"}
+)
+
+--map("i", "<C-k>", "<Up>", { desc = "Move up" })
+--
+---- multiple modes
+--map({ "i", "n" }, "<C-k>", "<Up>", { desc = "Move down" })
+--
+--map("n", "<leader>ff", ":Telescope <cr>")
+--
+---- mapping with a lua function
+--map("n", "<A-i>", function()
+--  require("nvchad.term").toggle({ pos = "sp", id ='abc' })
+--end, { desc = "Terminal toggle floating" })
+--
+--
+---- Disable mappings
+--local nomap = vim.keymap.del
+--
+--nomap("i", "<C-k>")
+--nomap("n", "<C-k>")
