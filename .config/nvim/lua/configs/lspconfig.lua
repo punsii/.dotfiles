f1b018986039f4988a20e3bd6e4ef9d5a84c9b03
Paul Menhart <paul_menhart@genua.de>
2024-06-03 15:55:17 +0200

    merge lspconfig.lua

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,12 @@
--- EXAMPLE 
+HOME = os.getenv("HOME")
+local config = require('nvchad.configs.lspconfig').defaults()
+
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
 local servers = { "html", "cssls" }
 
 -- lsps with default config
@@ -15,9 +18,69 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
+lspconfig.pyright.setup({
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
-}
+	filetypes = { "python" },
+})
+lspconfig.ruff_lsp.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+	filetypes = { "python" },
+})
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+	filetypes = { "rust" },
+	root_dir = util.root_pattern("Cargo.toml"),
+	settings = {
+		["rust_analyzer"] = {},
+	},
+})
+
+lspconfig.tsserver.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = HOME .. "/.npm-packages/lib/node_modules/@vue/typescript-plugin/",
+				languages = { "javascript", "typescript", "vue" },
+			},
+		},
+	},
+	filetypes = {
+		"javascript",
+		"typescript",
+		"vue",
+	},
+})
+lspconfig.volar.setup( {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
+
+-- lspconfig.vtsls.setup({})
+lspconfig.svelte.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
+lspconfig.nil_ls.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+})
+lspconfig.lua_ls.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  -- capabilities = "",
+})
