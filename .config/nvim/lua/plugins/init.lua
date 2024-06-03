df04b2f1ea1cf8858340ecc9b73286884c23ab26
Paul Menhart <paul_menhart@genua.de>
2024-06-03 16:34:07 +0200

    merge plugins.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,147 @@
-return {
+local plugins = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"c",
+				"vimdoc",
+				"css",
+				"go",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"nix",
+				"python",
+				"rust",
+				"svelte",
+				"typescript",
+				"vim",
+				"vue",
+			},
+		},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			git = {
+				enable = true,
+			},
+			renderer = {
+				highlight_git = "name",
+				icons = {
+					show = {
+						git = true,
+					},
+				},
+			},
+		},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_open_to_the_world = 1
+			vim.g.mkdp_open_ip = "127.0.0.1"
+			vim.g.mkdp_port = 8894
+			vim.g.mkdp_browser = "none"
+			vim.g.mkdp_echo_preview_url = 1
+		end,
+		ft = { "markdown" },
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = "mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		config = function(_, opts)
+			require("core.utils").load_mappings("dap")
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function(_, opts)
+			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
+			require("core.utils").load_mappings("dap_python")
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		ft = {
+			"*",
+		},
+		opts = function()
+			return require("configs.null-ls")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"bash-language-server",
+				"black",
+				"debugpy",
+				"mypy",
+				"nil",
+				"pyright",
+				"ruff-lsp",
+				"rust-analyzer",
+				"stylua",
+				"isort",
+				"prettierd",
+			},
+			-- "vtsls",
+			-- "vue-language-server",
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+      require('nvchad.configs.lspconfig').defaults()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		event = "VeryLazy",
+	},
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre', -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
@@ -36,3 +176,5 @@ return {
   -- 	},
   -- },
 }
+
+return plugins
