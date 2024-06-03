670024c0074a906e2ea6b34adb05e9e2c191105c
Paul Menhart <paul_menhart@genua.de>
2024-06-03 16:00:04 +0200

    merge chadrc.lua

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -2,15 +2,21 @@
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
+require("autocmds")
+
 local M = {}
 
+M.mappings = require("mappings")
+M.plugins = "plugins"
 M.ui = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	theme = "everforest",
+	tabufline = {
+		lazyload = false,
+	},
+	theme_toggle = { "everforest", "everforest_light" },
+	nvdash = {
+		load_on_startup = true,
+	},
 }
 
 return M
