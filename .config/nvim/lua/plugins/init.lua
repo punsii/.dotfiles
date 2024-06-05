5bc64432ef584f3b25cacf36ee2f848d8063d2e1
Paul Menhart <paul_menhart@genua.de>
2024-06-05 15:20:44 +0200

    add comment.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -143,6 +143,14 @@ local plugins = {
       require "configs.conform"
     end,
   },
+  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  },
 }
 
 return plugins
