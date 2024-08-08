34bfe0c694409246b203629bb74dd411d9cbb03a
Paul Menhart <paul_menhart@genua.de>
2024-08-08 16:50:18 +0200

    switch to nixfmt

--- .config/nvim/flake.nix
+++ .config/nvim/flake.nix
@@ -6,23 +6,31 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs = { self, nixpkgs, treefmt-nix }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      treefmtEval = treefmt-nix.lib.evalModule pkgs
-        {
-          # Used to find the project root
-          projectRootFile = "flake.nix";
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        # Used to find the project root
+        projectRootFile = "flake.nix";
 
-          # Enable the formatters
-          programs.stylua.enable = true;
-          programs.nixpkgs-fmt.enable = true;
-          programs.prettier = {
-            enable = true;
-            excludes = [ "data/*" "flake.lock" "lazy-lock.json" ];
-          };
+        # Enable the formatters
+        programs.stylua.enable = true;
+        programs.nixfmt.enable = true;
+        programs.prettier = {
+          enable = true;
+          excludes = [
+            "data/*"
+            "flake.lock"
+            "lazy-lock.json"
+          ];
         };
+      };
     in
     {
       formatter.x86_64-linux = treefmtEval.config.build.wrapper;
