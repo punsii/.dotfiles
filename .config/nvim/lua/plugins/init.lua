0423764ad4697e594cbebf124dae0efeec1f331a
Paul Menhart <paul_menhart@genua.de>
2025-02-10 12:36:19 +0100

    add mdx.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -45,6 +45,12 @@ local plugins = {
     },
   },
   {
+    "davidmh/mdx.nvim",
+    config = true,
+    event = "BufEnter *.mdx",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
+  {
     "iamcco/markdown-preview.nvim",
     ft = { "markdown" },
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
